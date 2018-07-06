97348d6011d51da3c5c7d1d31eb7e1da17532e3b
tobias <tobiasleongzhunmun@gmail.com>
2018-07-06 19:53:01 +0800

    part 2 complete

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+# this registers Question model to the admin view, making it editable!
+admin.site.register([Question, Choice])
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+	question_text = models.CharField(max_length = 200)
+	pub_date = models.DateTimeField('date published')
+
+	# change the representation of object
+	def __str__(self):
+		return self.question_text
+
+	# custom function
+	# check if pub_date is more than or equal to one day from "today"
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length = 200)
+	votes = models.IntegerField(default = 0)
+
+	# change the representation of object
+	def __str__(self):
+		return self.choice_text
